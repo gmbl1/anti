2dd615f3091593fe4076274c03233f370ce7c21f
gmbl1 <56491092+gmbl1@users.noreply.github.com>
2020-01-06 14:21:07 +0100

    Update pp.js

--- pp.js
+++ pp.js
@@ -307,7 +307,7 @@ client.on("reachLimit", (limit)=> {
 
 client.on('ready', function(){
     var ms = 2000 ;
-    var setGame = [' ',`🔥 Jh Gang 🔥`,` ❤ MER7BA BRJO3 ❤`,' 🧡 SAMURAI 🧡 ','🔰 SAMURAI 🔰'];
+    var setGame = ['🔥 Jh Gang 🔥',`🔥 MER7BA  🔥`,` ❤ BRJO3 ❤`,' 🧡 SAMURAI 🧡 ','🔰 SAMURAI 🔰'];
     var i = -1;
     var j = 0;
     setInterval(function (){
